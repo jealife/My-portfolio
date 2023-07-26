bb30109aeccbbed80480e6b3e1c4d8010dd93123
jealife <jeanguylanememiaghe@gmail.com>
2023-07-26 14:10:43 +0100

    new nav

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -12,34 +12,33 @@ import Home from "./Home";
 import About from "./About";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
+import Skills from "./Skills";
 import logo from "../Asset/Images/code-alt-regular-48.png";
 
 const Navigation = () => {
   return (
-    <Router classNameName="header">
-      <nav className="nav">
-        <div className="nav-menu">
-          <NavLink className="link-item" to="/">
-            Home
-          </NavLink>
-          <NavLink className="link-item" to="about">
-            About</NavLink>
-          <div className="nav-logo">
-            <Link className="link-item" to="/">
-              <img src={logo} className="logo" alt="Logo" />
-            </Link>
-          </div>
-          <NavLink className="link-item" to="/Portfolio">
-            Work
-          </NavLink>
-          <NavLink className="link-item" to="/Contact">
-            Contact
-          </NavLink>
-        </div>
-      </nav>
+    <Router >
+    <header>
+      <nav class="nav container">
+                <div className="logo">
+                    <Link className="link-item" to="/"><img src={logo} className="logo" alt="Logo" /> </Link>
+                </div>
+                <div className="menu-icon none">
+                    <box-icon name='menu-alt-right'></box-icon>
+                </div>
+                <div class="menu menu-dropdown menu-dropdown--fade-in none hide">
+                    <NavLink className="link-item" to="/">Home</NavLink>
+                    <NavLink className="link-item" to="about">About</NavLink>
+                    <NavLink className="link-item" to="/Skills">Skills</NavLink>
+                    <NavLink className="link-item" to="/Portfolio">Work</NavLink>
+                    <NavLink className="link-item" to="/Contact">Contact</NavLink>
+                </div>
+            </nav>
+    </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Skills" element={<Skills />} />
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Skills = ()=>{
+    return(
+        <div className="about-hero" id="about" transition-style="in:circle:top-right">
+            <h1 data-aos="flip-left" data-aos-duration="500">Skills</h1>
+        </div>
+    )
+}
+export default  Skills;
