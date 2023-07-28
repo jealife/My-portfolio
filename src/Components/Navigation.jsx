c56054d62c6ed9a7fc2ff2cd488398f64cb0b660
JEaLiFe <jealife.pictures@gmail.com>
2023-07-28 19:33:07 +0200

    Changes to be committed: 	modified:   public/index.html 	modified:   src/Asset/Style/Navigation.css 	modified:   src/Asset/Style/Responsive.css 	modified:   src/Components/Navigation.jsx

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "../Asset/Style/Navigation.css";
 import {
   BrowserRouter as Router,
@@ -15,34 +15,41 @@ import Contact from "./Contact";
 import Skills from "./Skills";
 import logo from "../Asset/Images/code-alt-regular-48.png";
 
-const Navigation = () => {
-  return (
-    <Router >
-    <header>
-      <nav class="nav container">
-                <div className="logo">
-                    <Link className="link-item" to="/"><img src={logo} className="logo" alt="Logo" /> </Link>
-                </div>
-                <div className="menu-icon none">
-                    <box-icon name='menu-alt-right'></box-icon>
-                </div>
-                <div class="menu menu-dropdown menu-dropdown--fade-in none hide">
-                    <NavLink className="link-item" to="/">Home</NavLink>
-                    <NavLink className="link-item" to="about">About</NavLink>
-                    <NavLink className="link-item" to="/Skills">Skills</NavLink>
-                    <NavLink className="link-item" to="/Portfolio">Work</NavLink>
-                    <NavLink className="link-item" to="/Contact">Contact</NavLink>
-                </div>
-            </nav>
-    </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Skills" element={<Skills />} />
-        <Route path="/Portfolio" element={<Portfolio />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+class Navigation extends Component{
+  state={clicked: false};
+  handleClick =()=>{
+    this.setState({clicked: !this.state.clicked})
+  }
+  render(){
+    return (
+      <Router >
+      <header>
+        <nav class="nav container">
+                  <div className="logo">
+                      <Link className="link-item" to="/"><img src={logo} className="logo" alt="Logo" /> </Link>
+                  </div>
+                  
+                  <div class={this.state.clicked ? "menu menu-dropdown showMenu": "menu menu-dropdown"}>
+                      <NavLink className="link-item" to="/">Home</NavLink>
+                      <NavLink className="link-item" to="about">About</NavLink>
+                      <NavLink className="link-item" to="/Skills">Skills</NavLink>
+                      <NavLink className="link-item" to="/Portfolio">Work</NavLink>
+                      <NavLink className="link-item" to="/Contact">Contact</NavLink>
+                  </div>
+                  <div className="menu-icon" onClick={this.handleClick}>
+                      <box-icon name={this.state.clicked ? "x": "menu"}></box-icon>
+                  </div>
+              </nav>
+      </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    );
+  }
 };
 export default Navigation;
